9ef485a0920615c4f8f6174bb223379e2ad66533
Nazarii <dzhaman.nazar2003@gmail.com>
2024-05-25 11:24:51 +0300

    add login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,40 @@
-const UserModel = require('../models/users');
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/users");
 
 const register = async (req, res, next) => {
-    const {name, email, password} = req.body;
-    try{
-        const user = await UserModel.findOne({email}).exec();
-        if(user !== null){
-            return res.status(409).send({message: "User already registered"}); 
-        }   await UserModel.create({name, email, password});
-            res.status(201).send({message: "Registration successfull"}); 
-    } catch(error){
-        next(error);
+  const { name, email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email }).exec();
+    if (user !== null) {
+      return res.status(409).send({ message: "User already registered" });
     }
-}
+    const passwordHash = await bcrypt.hash(password, 10);
+    await UserModel.create({ name, email, password: passwordHash });
+    res.status(201).send({ message: "Registration successfull" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = register;
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email }).exec();
+    if (user === null) {
+      return res
+        .status(401)
+        .send({ message: "Email or password is incorrect" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch !== true) {
+      return res
+        .status(401)
+        .send({ message: "Email or password is incorrect" });
+    }
+    res.send({ token: "TOKEN" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { register, login };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const register = require('../controllers/usersController');
+const {register, login} = require('../controllers/usersController');
 
 const usersRouter = express.Router();
 
 const jsonParser = express.json();
 
-usersRouter.post('/register', jsonParser, register)
+usersRouter.post('/register', jsonParser, register);
+usersRouter.post('/login', jsonParser, login);
 
 module.exports = usersRouter;
