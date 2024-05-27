0cfe555d50c529a92f6c9ffdabd35223ff0db361
Nazarii <dzhaman.nazar2003@gmail.com>
2024-05-27 10:51:51 +0300

    improve avatar

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const crypto = require('node:crypto');
-const path = require('node:path');
-const multer = require('multer');
+const upload = require('../middleware/upload');
 const {register, login, logout, uploadAvatar} = require('../controllers/usersController');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
 
 const usersRouter = express.Router();
 
 const jsonParser = express.json();
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "..", "tmp"));
-    },
-    filename: (req,file,cb) => {
-        const extname = path.extname(file.originalname);
-        const basename = path.basename(file.originalname, extname);
-        const suffix = crypto.randomUUID();
-        cb(null, `${basename}-${suffix}${extname}`);
-    }
-});
-
-const upload = multer({storage});
 
 usersRouter.post('/register', jsonParser, register);
 usersRouter.post('/login', jsonParser, login);
