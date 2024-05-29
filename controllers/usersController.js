8a1f2ce6ed0a46bb1ca1f22f3d2ee3dab060e855
Nazarii <dzhaman.nazar2003@gmail.com>
2024-05-29 14:59:15 +0300

    add verify with email

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,9 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const fs = require('node:fs/promises');
 const path = require('node:path');
+const crypto = require('node:crypto');
 const UserModel = require("../models/users");
+const sendEmail = require('../helpers/sendEmail');
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -12,7 +14,22 @@ const register = async (req, res, next) => {
       return res.status(409).send({ message: "User already registered" });
     }
     const passwordHash = await bcrypt.hash(password, 10);
-    await UserModel.create({ name, email, password: passwordHash });
+    const verificationToken = crypto.randomUUID();
+    await UserModel.create({ name, email, password: passwordHash, verificationToken });
+    await sendEmail({
+      to: email,
+      subject: `Welcome on board, ${name}`,
+      html: `
+              <p>To confirm your registration, please click on lick below</p>
+              <p>
+                <a href="http://localhost:8080/api/users/verify/${verificationToken}">Click here</a>
+              </p>
+            `,
+      text: `
+      To confirm your registration, please click on lick belown\n
+      http://localhost:8080/api/users/verify/${verificationToken}
+            `
+    });
     res.status(201).send({ message: "Registration successfull" });
   } catch (error) {
     next(error);
@@ -33,6 +50,9 @@ const login = async (req, res, next) => {
       return res
         .status(401)
         .send({ message: "Email or password is incorrect" });
+    }
+    if(user.verify !== true) {
+      return res.status(404).send({message: "Please verify your email"})
     }
     const token = jwt.sign(
       {
@@ -70,4 +90,18 @@ const uploadAvatar = async(req, res, next) => {
   }
 }
 
-module.exports = { register, login, logout, uploadAvatar };
+const verify = async(req, res, next) => {
+  try{
+    const {token} = req.params;
+    const user = await UserModel.findOne({verificationToken: token}).exec();
+    if(user === null) {
+      return res.status(401).send({message: "Invalid token"})
+    }
+      await UserModel.findByIdAndUpdate(user._id, {verify: true, verificationToken: null}).exec();
+    res.send({message: "User verified"});
+  } catch(error) {
+    next(error)
+  }
+}
+
+module.exports = { register, login, logout, uploadAvatar, verify };
